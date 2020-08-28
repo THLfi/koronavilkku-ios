787cb56ebe5b76eece026f4aeca4d01fa8dfe3a0
Alpertti Tirronen <alpertti.tirronen@solita.fi>
2020-08-28 10:51:53 +0300

    back to old path and removed debug

--- .github/docker-owasp-dependency-check-ios/entrypoint.sh
+++ .github/docker-owasp-dependency-check-ios/entrypoint.sh
@@ -2,6 +2,5 @@
 
 set -e
 
-ls -Rla /github/workspace/owasp
 
 sh -c "dependency-check.sh --enableExperimental --project '$GITHUB_REPOSITORY' --scan '$PWD/$INPUT_DERIVED_DATA_PATH' --suppression '$PWD/owasp_suppressions.xml' --failOnCVSS 1"
