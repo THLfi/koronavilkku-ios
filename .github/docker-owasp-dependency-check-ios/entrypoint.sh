5c7df3dd78bca524ce2145c101b53bce88ec63aa
Alpertti Tirronen <alpertti.tirronen@solita.fi>
2020-08-28 10:30:53 +0300

    relocated dependency-check + debug

--- .github/docker-owasp-dependency-check-ios/entrypoint.sh
+++ .github/docker-owasp-dependency-check-ios/entrypoint.sh
@@ -2,4 +2,6 @@
 
 set -e
 
+ls -Rla /github/workspace/owasp
+
 sh -c "dependency-check.sh --enableExperimental --project '$GITHUB_REPOSITORY' --scan '$PWD/$INPUT_DERIVED_DATA_PATH' --suppression '$PWD/owasp_suppressions.xml' --failOnCVSS 1"
