01dac40364bfc4dce0c64a8d6401a7d8e60c569b
Alpertti Tirronen <62981719+solita-alperttiti@users.noreply.github.com>
2020-08-28 13:02:23 +0300

    Add suppression.xml and automatize failure reports, fix cache issues (#7 - COVID-358)
    * added suppression and set check to fail if there is vulnerabilities, switched notification on with success for testing
    
    * switch to custom data dir for cache

--- .github/docker-owasp-dependency-check-ios/entrypoint.sh
+++ .github/docker-owasp-dependency-check-ios/entrypoint.sh
@@ -2,4 +2,5 @@
 
 set -e
 
-sh -c "dependency-check.sh --enableExperimental --project '$GITHUB_REPOSITORY' --scan '$PWD/$INPUT_DERIVED_DATA_PATH'"
+
+sh -c "dependency-check.sh --enableExperimental --project '$GITHUB_REPOSITORY' --scan '$PWD/$INPUT_DERIVED_DATA_PATH' --suppression '$PWD/owasp_suppressions.xml' --data '$PWD/owasp_dependency_check_data' --failOnCVSS 1"
