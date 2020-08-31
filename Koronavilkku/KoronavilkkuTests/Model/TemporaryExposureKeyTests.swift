fb08589cc7a6080cdb36bc4bd4ec2d959f975b3e
Tomi Lahtinen <tomi.lahtinen@solita.fi>
2020-08-31 14:48:08 +0300

    Added test cases

--- Koronavilkku/KoronavilkkuTests/Model/TemporaryExposureKeyTests.swift
+++ Koronavilkku/KoronavilkkuTests/Model/TemporaryExposureKeyTests.swift
@@ -4,13 +4,23 @@ import XCTest
 import XCTest
 
 class TemporaryExposureKeyTests: XCTestCase {
+    
+    static let keyCount = 14
 
     func testGenerateKeys() throws {
         
-        let keys = [String](repeating: "", count: 14).map { _ in
+        let keys = [String](repeating: "", count: TemporaryExposureKeyTests.keyCount).map { _ in
             TemporaryExposureKey.randomData(ofLength: TemporaryExposureKey.dataLength)
         }
-        XCTAssertEqual(14, keys.count)
-        print(keys)
+        XCTAssertEqual(keys.count, TemporaryExposureKeyTests.keyCount, "Correct amount of keys")
+        
+        // In theory this could fail if random generator produces the same data block
+        // twice but it's highly unlikely.
+        let uniqueKeys = Array(Set(keys))
+        XCTAssertEqual(uniqueKeys.count, keys.count, "All keys are unique")
+        
+        let lengths = Array(Set(uniqueKeys.map { $0.count }))
+        XCTAssertEqual(1, lengths.count, "All keys are same length")
     }
 }
+
