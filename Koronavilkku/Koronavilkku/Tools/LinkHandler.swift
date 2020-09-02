fc0b1a381146661adbcd6a10ac19cea655a071a2
Janne Käki <janne@awesomeness.pro>
2020-09-02 16:08:09 +0300

    Add inApp bool param to link handler function to determine whether to use in-app Safari or not

--- Koronavilkku/Koronavilkku/Tools/LinkHandler.swift
+++ Koronavilkku/Koronavilkku/Tools/LinkHandler.swift
@@ -6,18 +6,18 @@ class LinkHandler {
     
     static let shared = LinkHandler()
     
-    func open(_ url: URL) {
+    func open(_ url: URL, inApp: Bool) {
         let app = UIApplication.shared
-        guard var vc = app.windows.first?.rootViewController else {
+        
+        if inApp, var vc = app.windows.first?.rootViewController {
+            while vc.presentedViewController != nil {
+                vc = vc.presentedViewController!
+            }
+            
+            let safariController = SFSafariViewController(url: url)
+            vc.present(safariController, animated: true)
+        } else {
             app.open(url)
-            return
         }
-        
-        while vc.presentedViewController != nil {
-            vc = vc.presentedViewController!
-        }
-        
-        let safariController = SFSafariViewController(url: url)
-        vc.present(safariController, animated: true)
     }
 }
