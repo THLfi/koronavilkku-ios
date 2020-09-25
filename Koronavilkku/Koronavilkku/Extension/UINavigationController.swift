6c46fe4e0dbe18d17d449e3bd5503be5582fe7ed
Tomi Lahtinen <tomi.lahtinen@solita.fi>
2020-09-25 14:17:57 +0300

    Refactored UINavigationController extension to own file

---
+++ Koronavilkku/Koronavilkku/Extension/UINavigationController.swift
@@ -0,0 +1,55 @@
+import Foundation
+import UIKit
+
+extension UINavigationController {
+    func setDefaultStyle() {
+        navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .automatic
+        view.backgroundColor = UIColor.Secondary.blueBackdrop
+        navigationBar.tintColor = UIColor.Greyscale.black
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor.Secondary.blueBackdrop
+        appearance.shadowColor = .clear
+        appearance.largeTitleTextAttributes = [
+            .font: UIFont.heading1,
+            .foregroundColor: UIColor.Greyscale.black
+        ]
+        appearance.titleTextAttributes = [
+            .font: UIFont.labelPrimary,
+            .foregroundColor: UIColor.Greyscale.black
+        ]
+
+        appearance.setBackIndicatorImage(
+            UIImage.init(named: "arrow-left"),
+            transitionMaskImage: UIImage.init(named: "arrow-left")
+        )
+        
+        navigationBar.compactAppearance = appearance
+        navigationBar.scrollEdgeAppearance = appearance
+        navigationBar.standardAppearance = appearance
+    }
+    
+    var largeTitleFont: UIFont? {
+        get {
+            navigationBar.standardAppearance.largeTitleTextAttributes[.font] as? UIFont
+        }
+        
+        set {
+            guard let font = newValue, font !== largeTitleFont else {
+                return
+            }
+            
+            let appearance = navigationBar.standardAppearance.copy()
+
+            appearance.largeTitleTextAttributes = [
+                .font: font
+            ]
+            
+            navigationBar.standardAppearance = appearance
+            navigationBar.compactAppearance = appearance
+            navigationBar.scrollEdgeAppearance = appearance
+        }
+    }
+}
