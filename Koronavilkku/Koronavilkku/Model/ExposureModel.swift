600990439609e50bfc14cd51bc0c4429cafec8a0
Henry Jalonen <henry.jalonen@solita.fi>
2021-01-26 18:05:26 +0200

    Change exposure notification time parameters
    * Notification retention time is now adjusted to 14 days
    * Exposure detection interval is also changed to 14 days
    * Refactored and commented the code to be a bit more readable

--- Koronavilkku/Koronavilkku/Model/ExposureModel.swift
+++ Koronavilkku/Koronavilkku/Model/ExposureModel.swift
@@ -7,18 +7,21 @@ enum ExposureStatus: Equatable {
 }
 
 struct ExposureNotification: Codable {
-    /// How long the notification is being shown after the exposure
-    ///
-    /// The official exposure retention time is 10 * 24 hours from the time of exposure.
-    /// As we don't know the exact time of exposure, just the 24-hour window starting
-    /// from 00:00 UTC, we need to add one additional 24-hour window to make sure every
-    /// possible exposure is covered.
-    static let retentionTime: TimeInterval = .days(10 + 1)
+    typealias Days = Double
     
-    /// Detection duration is 10 days long
-    static let detectionIntervalDuration: TimeInterval = .days(9)
-    static let detectionIntervalStart: TimeInterval = .days(-10)
-
+    /// The number of days an exposure notification is being shown after the exposure
+    ///
+    /// This property could be moved to the configuration, but as our UI texts are
+    /// currently static, this is also fixed.
+    static let exposureNotificationValid: Days = 14
+    
+    /// The number of days exposure detection goes back in time to find potential exposures
+    ///
+    /// While this value could be determined from the EN configuration, V1 API does not
+    /// provide enough granularity we need. We can revisit this issue later once we've moved
+    /// to the V2 API.
+    static let exposureDetectionInterval: Days = 14
+    
     let detectedOn: Date
     let expiresOn: Date
     let detectionInterval: DateInterval
@@ -26,10 +29,29 @@ struct ExposureNotification: Codable {
     
     init(detectionTime: Date, latestExposureOn: Date, exposureCount: Int) {
         self.detectedOn = detectionTime
-        self.expiresOn = latestExposureOn.addingTimeInterval(ExposureNotification.retentionTime)
-        self.detectionInterval = DateInterval(start: detectionTime.addingTimeInterval(Self.detectionIntervalStart),
-                                              duration: Self.detectionIntervalDuration)
+        self.expiresOn = ExposureNotification.calculateRetentionTime(timeOfExposure: latestExposureOn)
+        self.detectionInterval = ExposureNotification.calculateDetectionInterval(from: detectionTime)
         self.exposureCount = exposureCount
+    }
+
+    /// Determines the exposure notification retention time
+    ///
+    /// The official exposure retention time is calculated from the time of exposure.
+    /// As we don't know the exact time of exposure, just the 24-hour window starting
+    /// from 00:00 UTC, we need to add one additional 24-hour window to make sure every
+    /// possible exposure is covered.
+    static func calculateRetentionTime(timeOfExposure: Date) -> Date {
+        timeOfExposure.addingTimeInterval(.days(Self.exposureNotificationValid + 1))
+    }
+    
+    /// Calculates the date range used to detect exposures from the detection time
+    ///
+    /// The interval does not contain the current day, as our current implementation
+    /// creates the batch files from the TEK's issued in the previous day. Therefore the
+    /// interval is "one day shorter".
+    static func calculateDetectionInterval(from detectionTime: Date) -> DateInterval {
+        .init(start: detectionTime.addingTimeInterval(.days(0 - Self.exposureDetectionInterval)),
+              duration: .days(Self.exposureDetectionInterval - 1))
     }
 }
 
@@ -60,8 +82,11 @@ extension Collection where Element : ENExposureInfo {
 ///
 /// - Important: Deprecated, use ExposureNotification instead
 struct Exposure: Codable {
-    // Keep using the old retention time here to avoid problems if the ExposureNotification
-    // retention time changes in the future
+    /// How long the notification is being shown after the exposure
+    ///
+    /// We're still using the old value here on purpose, because the retention
+    /// time is a computed property based on this value and this is the last
+    /// value we have shipped and therefore is the most likely expiration time.
     static let retentionTime: TimeInterval = .days(10 + 1)
     
     let date: Date
